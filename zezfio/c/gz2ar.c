c3651705e57e4f88c5dc3aaa52a538cecb7a9a13
TApplencourt <tapplencourt@gmail.com>
2016-09-03 12:53:16 +0200

    Cleaning c

--- zezfio/c/gz2ar.c
+++ zezfio/c/gz2ar.c
@@ -11,9 +11,10 @@
  *                                          
  */
 
-char   * extract_gz_into_buffer(const char *, const long unsigned int, const long unsigned int, int *);
-int    * ezfio_extract_int     (const char *, const long unsigned int, int *);
-
+void extract_gz_into_buffer(const char *, const long unsigned int, char * const);
+long unsigned int skip_lines(const char * buffer, const int);
+void ezfio_extract_int     (char *, const long unsigned int, int *);
+int fast_atoi (const char * str);
 
 /***
  *     ___                   ./                                 
@@ -21,113 +22,97 @@ int    * ezfio_extract_int     (const char *, const long unsigned int, int *);
  *     _|_ | | | |_) | (/_ | | | (/_ | | |_ (_|  |_ | (_) | | 
  *               |                                            
  */
-char * extract_gz_into_buffer(const char * filename,
-                              const long unsigned int nb_int_supposed,
-                              const long unsigned int chars_per_value,
-                              int *error_code) {
 
-  *error_code = 0;
-  /* And the header of ezfio is max of 100 character
-     10 Millions  collumn with one element in each*/
-  const long unsigned int header_max_size = 200;
+/* The requirements are:
+1. Input string contains only numeric characters, or is empty
+2. Input string represents a number from 0 up to INT_MAX
+*/
 
-  /* Number of bytes expected (with the \n) */
-  const long unsigned int bytes_expected = (1 + chars_per_value) * nb_int_supposed + header_max_size;
+ int fast_atoi( const char * str )
+{
+    int val = 0;
+    while( *str ) {
+        val = val*10 + (*str++ - '0');
+    }
+    return val;
+}
 
-  /* Allocate the buffer of char uncompresed*/
-  char * buffer = (char * ) malloc(bytes_expected);
-  if (buffer == NULL) {
-    fprintf(stderr, "Allocate memory for uncompressed buffer failed: %s.\n",
-                    strerror(errno));
-    *error_code = 1;
-    return buffer;
-  }
-
+void extract_gz_into_buffer(const char * filename,
+              const long unsigned int bytes_expected,
+              char * const buffer) {
 
   /* Open gzFile */
   const gzFile file = gzopen(filename, "r");
 
   if (!file) {
-    fprintf(stderr,
-      "gzopen of '%s' failed: %s.\n",
-      filename, strerror(errno));
-    *error_code = 1;
-    return buffer;
+    fprintf(stderr, "gzopen of '%s' failed: %s.\n",
+            filename, strerror(errno));
+    errno = -1;
+    return;
   }
 
   /* Extract */
   const int bytes_uncompresed_read = gzread(file, buffer, bytes_expected-1);
   if (!gzeof(file)) {
-    fprintf(stderr, "Error: gzread have not reach the end of file. Maybe ask for more integer (>%lu).\n",
-      nb_int_supposed);
-    *error_code = 1;
-    return buffer;
+    fprintf(stderr, "Error: Your buffer is to small for the uncompressed data. \n");
+    errno = -1;
+    return ;
   }
 
   buffer[bytes_uncompresed_read] = '\0';
-  return buffer;
 }
 
-int * ezfio_extract_int(const char * filename, const long unsigned int nb_int_supposed, int *error_code) {
-
-  *error_code = 0;
-
-  /* Create the buffer */
-  int * int_array = (int * ) malloc(sizeof(int) * nb_int_supposed);
-  if (int_array == NULL) {
-    fprintf(stderr, "Allocate memory failed: %s.\n",
-                      strerror(errno));
-     *error_code = 1;
-     return int_array;
-  }
-
-  /* Integer Case    
-     Let say we need 10 chars for each integer
-     unsigned long  4 bytes 0 to 4,294,967,295 */
-  char *buffer = extract_gz_into_buffer(filename, nb_int_supposed, 10, error_code);
-  if (*error_code)  {
-    return int_array;}
-
-  long unsigned int int_possitions = 0;
-  long unsigned int nb_int_read = 0;
+long unsigned int skip_lines(const char * buffer, const int number_of_line){
+  int ligne_read = 0;
   long unsigned int bytes_read = 0;
 
-  int skip_header = 0;
+  while (buffer[bytes_read]) {
+    if (buffer[bytes_read] == '\n') {
+        
+        if (ligne_read == number_of_line-1) break;
+        ligne_read ++;
+    }
+    bytes_read ++;
+    }
 
+    return bytes_read + 1;
+}
+
+void ezfio_extract_int(char * const buffer, const long unsigned int scalars_supposed, int * const int_array){
+
+  long unsigned int bytes_read = skip_lines(buffer, 2);
+
+
+  long unsigned int int_possitions = bytes_read;
+
+  long unsigned int scalars_read = 0;
   while (buffer[bytes_read]) {
 
     if (buffer[bytes_read] == '\n') {
 
-      if (nb_int_read > nb_int_supposed) {
-        fprintf(stderr, "Error: nb_int_read (%lu) > nb_int_supposed (%lu).\n",
-                        nb_int_read, nb_int_supposed);
-         *error_code = 1;
-          break;
+      if (scalars_read > scalars_supposed) {
+        fprintf(stderr, "Error: They are more value than (%lu) to read.\n",
+                        scalars_read);
+        errno = -1;
+        return;
       }
 
         buffer[bytes_read] = '\0';
+        int_array[scalars_read] = atoi( & buffer[int_possitions]);
+        scalars_read++;
 
-        if (skip_header < 2) {
-          skip_header++;
-        } else {
-          int_array[nb_int_read] = atoi( & buffer[int_possitions]);
-          nb_int_read++;
-        }
-        
         int_possitions = bytes_read + 1;
     }
     bytes_read++;
   }
 
-  if (nb_int_read < nb_int_supposed) {
-    fprintf(stderr, "Error: nb_int_read (%lu) < nb_int_supposed (%lu). You overshoot the number of integer.\n",
-                    nb_int_read, nb_int_supposed);
-    *error_code = 1;
-    return int_array;
+  if (scalars_read < scalars_supposed) {
+    fprintf(stderr, "Error: I read less value (%lu) than asked (%lu).\n",
+                    scalars_read, scalars_supposed);
+    errno = -1;
   }
 
-  free(buffer);
-  return int_array;
+  return;
 }
 
 int main( int argc, const char* argv[] )
@@ -135,18 +120,55 @@ int main( int argc, const char* argv[] )
 //  char *file =  "../input/bench.ezfio/bench/b1M.gz";
 //  long unsigned int nb_of_int = 10000000;
 
-  char *file =  "../input/bench.ezfio/bench/b100.gz";
-  long unsigned int nb_of_int = 100;
-  int error_code;
+  const long unsigned int header_max_size = 200;
+  const long unsigned int nb_value_supposed = 10000000;
+  const long unsigned int chars_per_value = 10;
 
-  int *data = ezfio_extract_int(file, nb_of_int, &error_code);
+  /* Number of bytes expected (with the \n) */
+  const long unsigned int bytes_expected = (1 + chars_per_value) * nb_value_supposed + header_max_size;
 
-  printf("Error code: %d\n", error_code );
+  /* Allocate the buffer of char uncompresed*/
+  char * buffer = (char * ) malloc(bytes_expected);
+  if (buffer == NULL) {
+    fprintf(stderr, "Allocate memory for uncompressed buffer failed: %s.\n",
+                    strerror(errno));
+  }
 
-//  for(int i=0;i<nb_of_int;++i) {
-//    printf("%d\n", data[i]);
-//  }
+  char *file =  "../input/bench.ezfio/bench/b100M.gz";
+  extract_gz_into_buffer(file,bytes_expected,buffer);
   
-  free(data);
+
+  if (errno != 0) {
+    printf("%s\n", "failed");
+  }
+  else {
+    printf("%s\n", "ok");
+  }
+
+  /* Create the buffer */
+  int * int_array = (int * ) malloc(sizeof(int) * nb_value_supposed);
+  if (int_array == NULL) {
+    fprintf(stderr, "Allocate memory failed: %s.\n",
+                      strerror(errno));
+  }
+
+
+   ezfio_extract_int(buffer,nb_value_supposed,int_array);
+
+  if (errno != 0) {
+    printf("%s\n", "failed");
+  }
+  else {
+    printf("%s\n", "ok");
+  }
+
+
+  for(int i=0;i<10;++i) {
+      printf("%d\n", int_array[i]);
+  }
+  
+  free(int_array);
+  free(buffer);
+
   return 0;
 }
